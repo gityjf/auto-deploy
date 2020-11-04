cf198d43253b534768d64c81873b11c0281e0dd3
gityjf <2211675158@qq.com>
2020-11-04 10:23:23 +0800

    test1

---
+++ src/main/java/com/util/LocalTest.java
@@ -0,0 +1,13 @@
+package com.util;
+
+/**
+ * @program: auto-deploy
+ * @author: yjf
+ * @create: 2020-11-04 10:18
+ **/
+public class LocalTest {
+
+    public static void main(String[] args) {
+
+    }
+}
